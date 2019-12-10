f7735c773303f988339de249577531545c800141
Ermat <ealymbaev@gmail.com>
2019-12-10 15:11:01 +0600

    Change podspec to support static library mode

--- CryptoBLS/Source/CryptoBLS.swift
+++ CryptoBLS/Source/CryptoBLS.swift
@@ -1,10 +1,9 @@
 import Foundation
-import CryptoBLS.Private
 
 public struct CryptoBLS {
 
     public static func verify(messageDigest: Data, pubKey: Data, signature: Data) -> Bool {
-        return BLSKey.verify(messageDigest, publicKey: pubKey, signature: signature)
+        BLSKey.verify(messageDigest, publicKey: pubKey, signature: signature)
     }
 
 }
